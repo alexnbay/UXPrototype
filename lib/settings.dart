110d30dbebbe57c651c6f0d8046a9d46c85578a3
baychub <31420038+baychub@users.noreply.github.com>
2022-12-10 11:13:41 -0700

    Tweaked Settings for short devices
    Changed Column to Listview to not cut off color schemes. Centered Text.

--- lib/settings.dart
+++ lib/settings.dart
@@ -91,67 +91,65 @@ class _SettingsRouteState extends State<SettingsRoute> {
           backgroundColor: HomePageState.darkColor,
           automaticallyImplyLeading: false
       ),
-      body: Column(
-          children: [
-            SizedBox(height: correct(20)),
-            Text('Number of Bars in each Graph', style: TextStyle(fontSize: correct(21))),
-            SizedBox(height: correct(20)),
-            Row(children: [
-              SizedBox(width: correct(20)),
-              const Text('5'),
-              SizedBox(width: correct(165)),
-              const Text('10'),
-              SizedBox(width: correct(165)),
-              const Text('15')
-            ],),
-            Slider(
-              value: graphColumnsLocal.toDouble(),
-              min: 5.0,
-              max: 15.0,
-              label: graphColumnsLocal.toString(),
-              divisions: 10,
-              onChanged: (double newValue) {
-                setState(() {
-                  updateGraphColumns(newValue.toInt());
-                });
-              },
-            ),
-            SizedBox(height: correct(40)),
-            Text('Choose a Color Scheme', style: TextStyle(fontSize: correct(21))),
-            SizedBox(height: correct(30)),
-            Row(
+      body: ListView(
               children: [
-                SizedBox(width:correct(20)),
-                getColorBoxes(const Color(0xff1c286c),
-                  const Color(0xff3243a2), 
-                  const Color(0xff7580be),
-                  const Color(0xffefeff8)),
-                SizedBox(width: correct(20)),
-                getColorBoxes(const Color(0xff712828),
-                    const Color(0xff9b3333),
-                    const Color(0xffbe6b6b),
-                    const Color(0xfffde8e8)),
-              ],
-            ),
-            SizedBox(height: correct(20)),
-            Row(
-              children: [
-              SizedBox(width:correct(20)),
-              getColorBoxes(const Color(0xff261060),
-                  const Color(0xff7621ac),
-                  const Color(0xffb157a9),
-                  const Color(0xffFDEDF3)),
-              SizedBox(width: correct(20)),
-              getColorBoxes(const Color(0xff102360),
-                  const Color(0xff5F4DC1),
-                  const Color(0xffD9A06F),
-                  const Color(0xffFFF8E0)),
-            ],
+                SizedBox(height: correct(20)),
+                Center(child: Text('Number of Bars in each Graph', style: TextStyle(fontSize: correct(21)))),
+                SizedBox(height: correct(20)),
+                Row(children: [
+                  SizedBox(width: correct(20)),
+                  const Text('5'),
+                  SizedBox(width: correct(165)),
+                  const Text('10'),
+                  SizedBox(width: correct(165)),
+                  const Text('15')
+                ],),
+                Slider(
+                  value: graphColumnsLocal.toDouble(),
+                  min: 5.0,
+                  max: 15.0,
+                  label: graphColumnsLocal.toString(),
+                  divisions: 10,
+                  onChanged: (double newValue) {
+                    setState(() {
+                      updateGraphColumns(newValue.toInt());
+                    });
+                  },
+                ),
+                SizedBox(height: correct(40)),
+                Center(child: Text('Choose a Color Scheme', style: TextStyle(fontSize: correct(21)))),
+                SizedBox(height: correct(30)),
+                Row(
+                  children: [
+                    SizedBox(width:correct(20)),
+                    getColorBoxes(const Color(0xff1c286c),
+                        const Color(0xff3243a2),
+                        const Color(0xff7580be),
+                        const Color(0xffefeff8)),
+                    SizedBox(width: correct(20)),
+                    getColorBoxes(const Color(0xff712828),
+                        const Color(0xff9b3333),
+                        const Color(0xffbe6b6b),
+                        const Color(0xfffde8e8)),
+                  ],
+                ),
+                SizedBox(height: correct(20)),
+                Row(
+                  children: [
+                    SizedBox(width:correct(20)),
+                    getColorBoxes(const Color(0xff261060),
+                        const Color(0xff7621ac),
+                        const Color(0xffb157a9),
+                        const Color(0xffFDEDF3)),
+                    SizedBox(width: correct(20)),
+                    getColorBoxes(const Color(0xff102360),
+                        const Color(0xff5F4DC1),
+                        const Color(0xffD9A06F),
+                        const Color(0xffFFF8E0)),
+                  ],
 
-            )
-
-
-          ]
+                )
+              ]
       ),
       bottomNavigationBar: BottomNavigationBar(
           iconSize: correct(48),
